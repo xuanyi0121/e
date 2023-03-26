85040944a9dcf029e55467fec4feb42862cd5484
e <110735940+xuanyi0121@users.noreply.github.com>
2023-03-26 21:28:10 +0800

    Create zhuanhuan.py

---
+++ zhuanhuan.py
@@ -0,0 +1,29 @@
+import re
+import time
+from urllib import parse
+
+from pyrogram.types import Message
+
+from pagermaid.listener import listener
+from pagermaid.utils import client as http_client, alias_command
+
+backend = "https://api.gdmm.ml/"
+config = "https://paste.wmlabs.net/raw/9b02b8b11b4a"
+
+@listener(is_plugin=True, outgoing=True, command=alias_command("zhuanhuan"),
+          description='转换节点和订阅链接',
+          parameters='<url / node>')
+async def convertsub(_, msg: Message):
+    try:
+        message_raw = msg.reply_to_message and (msg.reply_to_message.caption or msg.reply_to_message.text) or (msg.caption or msg.text)
+        await msg.edit('转换中...')
+        url_list = re.findall("[-A-Za-z0-9+&@#/%?=~_|!:,.;]+://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]", message_raw)
+        url_list = list(set(url_list))
+        result = "[[订阅链接]]("+ backend + "sub?target=clash&url=" + parse.quote_plus(url_list[0])
+        for url in url_list[1:]:
+            if len(url) != 0:
+                result = result + "|" + parse.quote_plus(url)
+        result = result + "&insert=false&config=" + parse.quote_plus(config) + "&emoji=true&list=false&tfo=false&expand=true&scv=true&fdn=false&new_name=true)"
+        await msg.edit(result)
+    except:
+        await msg.edit('参数错误')
